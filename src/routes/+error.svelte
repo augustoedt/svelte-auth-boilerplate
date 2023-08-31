af0434779cb933a3499a77923d690861ba3b3e48
Augusto Eduardo <augustoedt@gmail.com>
2023-08-31 10:41:34 -0300

    update routes

---
+++ src/routes/+error.svelte
@@ -0,0 +1,24 @@
+<div>
+    <h1>¯\_(ツ)_/¯</h1>
+    <p>Page not found</p>
+</div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        height: 250px;
+    }
+
+    div h1 {
+        font-size: 3rem;
+        margin: 0;
+    }
+    div p {
+        font-size: 1.5rem;
+        font-weight: 700;
+        margin: 0;
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,50 @@
+<div>
+	<nav class="">
+		<ul>
+			<li><a href="/">Home</a></li>
+		</ul>
+		<div class="space" />
+		<ul>
+			<li><a href="/login">Login</a></li>
+			<li><a href="/register">Register</a></li>
+		</ul>
+	</nav>
+	<div class="body-layout">
+		<slot />
+	</div>
+</div>
+
+<style>
+	nav > ul {
+		display: flex;
+		font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+			sans-serif;
+		font-weight: 700;
+		list-style: none;
+		padding: 0;
+		gap: 1rem;
+	}
+	.space {
+		flex-grow: 1;
+	}
+
+	nav {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding: 1rem;
+		max-width: 650px;
+		margin: auto;
+	}
+
+	.body-layout {
+		max-width: 650px;
+		margin: auto;
+		padding: 1rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: #000;
+	}
+</style>
